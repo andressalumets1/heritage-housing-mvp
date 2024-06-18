b3e827f5ccaf3b5e1093bac9a5b4a56fbffe7c88
Andres Salumets <rnfi8mkz@students.codeinstitute.net>
2024-06-18 16:36:38 +0000

    Finish correlations page with plots and insights

--- app_pages/page_correlation.py
+++ app_pages/page_correlation.py
@@ -37,6 +37,20 @@ def heatmap_pps(df, threshold, figsize, font_annot):
     plt.ylim(len(df.columns), 0)
     st.pyplot(fig)
 
+def plot_scatter(df, variables, target='SalePrice'):
+    for var in variables:
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.scatterplot(data=df, x=var, y=target, ax=ax)
+        plt.title(f'{var} vs {target}')
+        st.pyplot(fig)
+
+def plot_box(df, variables, target='SalePrice'):
+    for var in variables:
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.boxplot(data=df, x=var, y=target, ax=ax)
+        plt.title(f'{var} vs {target}')
+        st.pyplot(fig)
+
 def page_correlation_body():
     """
     Display the correlated features.
@@ -44,6 +58,19 @@ def page_correlation_body():
     df3 = load_housing_cleaned()
     df_ohe = preprocess_data(df3)
     df_corr_spearman, df_corr_pearson, pps_matrix = calculate_corr_pps(df_ohe)
+
+    variables_to_study = [
+        'GrLivArea', 'YearBuilt', 'GarageArea', 'OpenPorchSF',
+        'TotalBsmtSF', '1stFlrSF', 'YearRemodAdd', 'GarageYrBlt',
+    ]
+
+    categorical_vars = ['OverallQual', 'KitchenQual_Ex', 'KitchenQual_Gd']
+
+    # EDA visualizations
+    st.subheader("Exploratory Data Analysis")
+    df_eda = df3.filter(variables_to_study + ['SalePrice'])
+
+    st.write(f"The most correlated variables to Sale Price are: {variables_to_study}")
 
     st.header("""Housing prices correlation study""")
     st.info("""
@@ -86,13 +113,68 @@ def page_correlation_body():
         heatmap_corr(df_corr_pearson, corr_threshold, figsize, font_annot)
         st.write("PPS Heatmap")
         heatmap_pps(pps_matrix, pps_threshold, figsize, font_annot)
+        st.subheader("Summary")
+        st.write("""
+        After correlation heatmaps between variables .corr method was
+        used, the return value was a pandas series, and the first items was the
+        correlation between SalePrice and SalePrice. Since it was correlation between
+        the target variable(SalePrice) itself then [1:] was used to exclude it from
+        the series. Next, the remaining values were sorted considering the absolute value,
+        which was done by setting key=abs. Then top 11 values were going to be shown.
+        Only positive values were taken from both Spearman and Pearson correlations against
+        SalePrice. After that they were combined and the result was:
+        
+        1. OverallQual (0.809829)
+        2. GrLivArea (0.731310)
+        3. YearBuilt (0.652682)
+        4. GarageArea (0.649438)
+        5. TotalBsmtSf (0.636999)
+        6. 1stFlrSF (0.620743)
+        7. YearRemodAdd (0.571159)
+        8. GarageYrBlt (0.563256)
+        9. KitchenQual_Ex (0.504094)
+        10. KitchenQual_Gd (0.478583)
+        11. OpenPorchSF (0.477889)
+        """)
 
+    if st.checkbox("""Scatter Plots with the most important continous numerical variables
+    against the SalePrice"""):
+        plot_scatter(df_eda, variables_to_study)
+        st.subheader("Summary of Insights:")
+        st.write("""
+        1. Large first-floor areas are associated with higher sale prices.
+        2. Larger garage areas generally lead to higher sale prices.
+        3. There is clear historical trend showing increased value for more
+        recently built garages.
+        4. Larger above-ground living areas are associated with higher sale prices.
+        5. Slight increase in sale prices with larger open porch areas. Wide spread
+        and variability in sale prices for similar porch sizes. Many properties
+        with zero open porch area clustering at lower sale prices.
+        6. Larger basement areas are associated with higher sale prices. High-value
+        properties with extensive basements.
+        7. Not surprisingly, houses built more recently generally have higher sale prices.
+        Significant increase in sale prices for houses built after 1980, especially after 2000.
+        Some older houses have high sale prices, possibly due to renovations or historical value.
+        8. More recent remodels and additions are associated with higher sale prices. 
+        Notable increase in sale prices for houses remodeled after 2000.
+        """)
 
-    if st.checkbox("House Prices per Variable"):
-        house_price_per_variable(df3)
-
-    if st.checkbox("Distribution of Variable"):
-        plot_target_hist(df3, 'SalePrice')
+    if st.checkbox("""Box plots with the most important categorical variables
+    against the SalePrice"""):
+        plot_box(df_ohe, categorical_vars)
+        st.subheader("Box Plot Insights:")
+        st.write("""
+        1. Kitchen quality:
+        * Both excellent and good kitchen quality significantly contribute to higher
+        sale prices. However the impact is more substantial for excellent kitchen quality.
+        2. Overall quality:
+        * Overall quality has a strong positive correlation with sale prices, showing
+        that higher overall quality ratings generally lead to higher sale prices.
+        3. Market value:
+        * Houses with higher kitchen quality and overall quality not only comman higher median
+        prices but also show greater variability and a higher propensity for outliers in the 
+        upper price range.
+        """)
 
 page_correlation_body()
 
