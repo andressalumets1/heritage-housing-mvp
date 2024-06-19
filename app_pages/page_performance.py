fb6884b4adab5b5dcc087608705762222c4efeb6
Andres Salumets <rnfi8mkz@students.codeinstitute.net>
2024-06-19 12:01:36 +0000

    Update performance page

--- app_pages/page_performance.py
+++ app_pages/page_performance.py
@@ -2,30 +2,10 @@ import streamlit as st
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
-import seaborn as sns
-from src.data_management import load_pkl_file
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-import numpy as np
-
-st.set_page_config(page_title="ML Pipeline Performance", layout="wide")
 
 def load_data(file_path):
     return pd.read_csv(file_path)
-
-X_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_train.csv")
-y_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/y_train.csv")
-X_test = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_test.csv")
-y_test = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/y_test.csv")
-best_pipeline_regressor = joblib.load("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/regression_pipeline.pkl")
-
-st.title("ML Pipeline Performance Dashboard")
-st.header("Data Overview")
-st.write("### Training Data")
-st.write(X_train.head())
-st.write("### Test Data")
-st.write(X_test.head())
-
-st.header("Model Performance Metrics")
 
 def regression_performance(X_train, y_train, X_test, y_test, pipeline):
     st.subheader("Model Evaluation")
@@ -39,10 +19,35 @@ def regression_evaluation(X, y, pipeline):
     st.write('R2 Score:', r2_score(y, prediction).round(3))
     st.write('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
     st.write('Mean Squared Error:', mean_squared_error(y, prediction).round(3))
-    st.write('Root Mean Squared Error:', np.sqrt(mean_squared_error(y, prediciton)).round(3))
+    st.write('Root Mean Squared Error:', mean_squared_error(y, prediction, squared=False).round(3))
     st.write("\n")
 
-regression_performance(X_train, y_train, X_test, y_test, best_pipeline_regressor)
+def page_performance_body():
+    X_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_train.csv")
+    y_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/y_train.csv")
+    X_test = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_test.csv")
+    y_test = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/y_test.csv")
+    best_pipeline_regressor = joblib.load("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/regression_pipeline.pkl")
+    feature_importance = plt.imread('/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/feature_importance.png')
 
-st.header("Feature Importance")
-df_feature_importance = pd.read_csv()
+    st.title("ML Pipeline Performance Dashboard")
+    st.header("Data Overview")
+    st.write("### Training Data")
+    st.write(X_train.head())
+    st.write("### Test Data")
+    st.write(X_test.head())
+
+    st.header("Model Performance Metrics")
+    regression_performance(X_train, y_train, X_test, y_test, best_pipeline_regressor)
+
+    # show best features
+    st.header("* The features the model was trained on and their importance")
+    st.write(X_train.columns.to_list())
+
+    st.image(feature_importance, caption='Feature Importance')
+    st.write("---")
+
+    # show pipeline steps
+    st.header("* ML pipeline to predict sales prices of houses")
+    st.code(best_pipeline_regressor)
+    st.write("---")
