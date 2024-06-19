edb6b81f583c30f1608e39a6a7214a55860b4425
Andres Salumets <rnfi8mkz@students.codeinstitute.net>
2024-06-19 12:28:19 +0000

    Conclude performance page

--- app_pages/page_performance.py
+++ app_pages/page_performance.py
@@ -5,16 +5,33 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
 def load_data(file_path):
+    """
+    Loads data from a CSV file located at 'file_path
+    using 'pandas' and returns it as a DataFrame.
+    """
     return pd.read_csv(file_path)
 
 def regression_performance(X_train, y_train, X_test, y_test, pipeline):
+    """
+    Evaluates the performance of a regression model pipeline on both training
+    and test datasets. It calls 'regression_evaluation' function to compute and display
+    metrics.
+    """
     st.subheader("Model Evaluation")
     st.write("* Train Set")
     regression_evaluation(X_train, y_train, pipeline)
     st.write("* Test Set")
     regression_evaluation(X_test, y_test, pipeline)
+    st.write("""
+    The model effectively fulfilled the client's proposed criteria of R2 = 0.75,
+    both on training and test sets.
+    """)
 
 def regression_evaluation(X, y, pipeline):
+    """
+    Computes and displays regression evaluation metrics
+    for predictions made by the 'pipeline'.
+    """
     prediction = pipeline.predict(X)
     st.write('R2 Score:', r2_score(y, prediction).round(3))
     st.write('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
@@ -23,6 +40,12 @@ def regression_evaluation(X, y, pipeline):
     st.write("\n")
 
 def page_performance_body():
+    """
+    Constructs a streamlit web application page that displays various sections of
+    model performance metrics and details. It loads training and test data, the best
+    performing regression pipeline 'best_pipeline_regressor', and visualizations like
+    'feature_importance' plot.
+    """
     X_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_train.csv")
     y_train = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/y_train.csv")
     X_test = load_data("/workspace/heritage-housing-mvp/outputs/ml_pipeline/predict_price/v1/X_test.csv")
@@ -51,3 +74,25 @@ def page_performance_body():
     st.header("* ML pipeline to predict sales prices of houses")
     st.code(best_pipeline_regressor)
     st.write("---")
+
+    st.header("* Conclusion")
+    st.write("""
+    In this project, it was developed a robust machine learning pipeline to predict house 
+    prices using various regression models. It was started by implementing extensive data 
+    cleaning and feature engineering steps. The core of the project involved hyperparameter 
+    optimization through GridSearchCV, where it was tested multiple models and 
+    configurations to identify the best-performing model.
+
+    The Gradient Boosting Regressor initially showed promising results, 
+    but further optimization revealed that the Extra Trees Regressor with specific 
+    hyperparameters provided the best performance, exceeding the business requirement 
+    of an R2 score of at least 0.75 on both the training and test sets. 
+    It was identified the most influential features and refit the model using these key 
+    predictors. Finally, the optimized model and relevant datasets were saved for 
+    future use.
+
+    This structured approach not only ensured the creation of an accurate and reliable 
+    predictive model but also highlighted the importance of feature selection and model 
+    tuning in achieving superior performance. The project demonstrates the effectiveness 
+    of a systematic ML pipeline in solving realworld regression problems.
+    """)
